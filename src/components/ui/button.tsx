125ad051b93d71fdea485ddb8676572ccc26b3de
Faisal Khrisan <faisalkhrisan@gmail.com>
2025-10-04 15:37:59 +0000

    the hovering should be black for the whith mod e (_for element <Header>_

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border border-input bg-transparent hover:bg-black hover:text-white dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-white dark:hover:text-black",
+        ghost: "hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
