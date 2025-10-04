2af2914dbeb08253fe65816dd637be6b0cc58f7c
Faisal Khrisan <faisalkhrisan@gmail.com>
2025-10-04 11:31:12 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <LanguageProvider>
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
           <head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
